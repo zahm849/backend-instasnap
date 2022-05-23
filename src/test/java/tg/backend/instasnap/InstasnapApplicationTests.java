f0695cb6cdfa1a11b5acf595caa75cd0e57719b3
unknown <you@example.com>
2022-05-23 18:11:41 +0200

    creation du projet instasnap sur git

---
+++ src/test/java/tg/backend/instasnap/InstasnapApplicationTests.java
@@ -0,0 +1,13 @@
+package tg.backend.instasnap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstasnapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
